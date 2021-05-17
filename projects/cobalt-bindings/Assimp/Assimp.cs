cbce39f112346c5bd1a1f65f74c6d23528082beb
Roderick Griffioen <roderick.griffioen@gmail.com>
2021-05-17 13:27:46 +0200

    Added Assimp binding

---
+++ projects/cobalt-bindings/Assimp/Assimp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Cobalt.Bindings.Assimp
+{
+    public static class Assimp
+    {
+        #region DLL Loading
+#if COBALT_PLATFORM_WINDOWS
+        public const string LIBRARY = "bin/Assimp@5.0.0-native-bindings.dll";
+#elif COBALT_PLATFORM_MACOS
+        public const string LIBRARY = "bin/Assimp@5.0.0-native-bindings";
+#else
+        public const string LIBRARY = "bin/Assimp@5.0.0-native-bindings";
+#endif
+        #endregion
+
+        [DllImport(LIBRARY, EntryPoint = "assimp_test", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void AssimpTest();
+    }
+}

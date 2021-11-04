7b6753275c2989f34ab8123440c190c797157b2a
Roderick Griffioen <roderick.griffioen@gmail.com>
2021-11-04 22:43:34 +0100

    Render graph attachments and dependencies

--- projects/cobalt/Graphics/RenderGraph.cs
+++ projects/cobalt/Graphics/RenderGraph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Cobalt.Core;
 using static Cobalt.Bindings.Vulkan.VK;
 
@@ -28,37 +29,107 @@ namespace Cobalt.Graphics
             public RenderPassInfo? passInfo;
             public ComputePassInfo? computeInfo;
             public IPass pass;
+
+            public List<string> inputAttachments;
+            public List<string> colorAttachments;
+            public string depthAttachment;
+            public List<string> uniformBuffers;
+            public List<string> storageBuffers;
         }
 
         private DirectedAcyclicGraph<PassInfo, PassDependencyInfo> _graph = new DirectedAcyclicGraph<PassInfo, PassDependencyInfo>();
 
+        private readonly List<PassInfo> _graphicsPasses = new List<PassInfo>();
+        private readonly List<PassInfo> _computePasses = new List<PassInfo>();
+
+        private readonly List<Tuple<PassDependencyInfo, IPass, IPass>> _dependencies =
+            new List<Tuple<PassDependencyInfo, IPass, IPass>>();
+
+        private readonly Dictionary<IPass, PassInfo> _passInfos = new Dictionary<IPass, PassInfo>();
+
         public void AddPass(IPass pass, string name)
         {
-            PassInfo info = new PassInfo();
-            info.name = name;
-            info.pass = pass;
+            PassInfo info = new PassInfo
+            {
+                pass = pass, 
+                name = name
+            };
+
             switch (pass.GetType())
             {
                 case IPass.PassType.Compute:
-                    info.computeInfo = new ComputePassInfo();
+                    _computePasses.Add(info);
                     break;
                 case IPass.PassType.Graphics:
-                    info.passInfo = new RenderPassInfo();
+                    _graphicsPasses.Add(info);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
 
-            _graph.AddVertex(info);
+            _passInfos[pass] = info;
         }
 
         public void AddDependency(IPass source, IPass dest, PassDependencyInfo info)
         {
-            //_graph.AddEdge(info, source, dest);
+            _dependencies.Add(new Tuple<PassDependencyInfo, IPass, IPass>(info, source, dest));
+        }
+
+        public void AddInputAttachment(IPass pass, string name)
+        {
+            _passInfos[pass].inputAttachments.Add(name);
+        }
+
+        public void AddColorAttachment(IPass pass, string name)
+        {
+            _passInfos[pass].colorAttachments.Add(name);
+        }
+
+        public void SetDepthAttachment(IPass pass, string name)
+        {
+            var info = _passInfos[pass];
+            info.depthAttachment = name;
+            _passInfos[pass] = info;
+        }
+
+        public void AddUniformBuffer(IPass pass, string name)
+        {
+            _passInfos[pass].uniformBuffers.Add(name);
+
+        }
+
+        public void AddStorageBuffer(IPass pass, string name)
+        {
+            _passInfos[pass].storageBuffers.Add(name);
         }
 
         public void Build()
         {
+            foreach(var (pass, info) in _passInfos)
+            {
+                foreach (string inputAtt in info.inputAttachments)
+                {
+                    foreach (var (otherPass, otherInfo) in _passInfos)
+                    {
+                        if (otherPass == pass)
+                            continue;
+
+                        if (!otherInfo.colorAttachments.Contains(inputAtt) && otherInfo.depthAttachment != inputAtt)
+                            continue;
+
+                        // Match found, check dependencies
+                        foreach (var (depInfo, source, dest) in _dependencies)
+                        {
+                            if ((source == pass && dest == otherPass) || (dest == pass && source == otherPass))
+                            {
+                                // Found the corresponsing dependencyinfo
+                                // Do shit with this
+                            }
+                        }
+                    }
+                }
+            }
+
             // build resources
             // do topo sort on passinfos that are renderpassinfo graphs
             // use to build vkrenderpass
